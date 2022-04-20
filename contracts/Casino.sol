da6acb1eea9a81b868b165f2c25a49d83ff1c0ad
ujjual <ujjual72@gmail.com>
2022-04-20 23:36:00 +0530

    b

--- contracts/Casino.sol
+++ contracts/Casino.sol
@@ -1,2 +1,17 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
+
+contract Casino {
+    address public owner;
+
+    constructor {
+        owner = msg.sender;
+    }
+
+    function kill() public {
+        if(msg.sender == owner){
+            selfdestruct(owner);
+        }
+    }
+
+}
