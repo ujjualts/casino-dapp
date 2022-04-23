8e842d866ae8f914892e7ac99190422d9f131a83
ujjual <ujjual72@gmail.com>
2022-04-23 21:29:48 +0530

    error class

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,46 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-
-const Home: NextPage = () => {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+class App extends React.Component {
+  constructor(props){
+     super(props)
+     this.state = {
+        lastWinner: 0,
+        timer: 0
+     }
+  }
+voteNumber(number){
+     console.log(number)
+  }
+render(){
+     return (
+        <div className="main-container">
+           <h1>Bet for your best number and win huge amounts of Ether</h1>
+<div className="block">
+              <h4>Timer:</h4> &nbsp;
+              <span ref="timer"> {this.state.timer}</span>
+           </div>
+<div className="block">
+              <h4>Last winner:</h4> &nbsp;
+              <span ref="last-winner">{this.state.lastWinner}</span>
+           </div>
+<hr/>
+<h2>Vote for the next number</h2>
+           <ul>
+              <li onClick={() => {this.voteNumber(1)}}>1</li>
+              <li onClick={() => {this.voteNumber(2)}}>2</li>
+              <li onClick={() => {this.voteNumber(3)}}>3</li>
+              <li onClick={() => {this.voteNumber(4)}}>4</li>
+              <li onClick={() => {this.voteNumber(5)}}>5</li>
+              <li onClick={() => {this.voteNumber(6)}}>6</li>
+              <li onClick={() => {this.voteNumber(7)}}>7</li>
+              <li onClick={() => {this.voteNumber(8)}}>8</li>
+              <li onClick={() => {this.voteNumber(9)}}>9</li>
+              <li onClick={() => {this.voteNumber(10)}}>10</li>
+           </ul>
         </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
-    </div>
-  )
+     )
+  }
 }
 
 export default Home
